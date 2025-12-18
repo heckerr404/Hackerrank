04e45554fac857ed9ade1b36f3d3b94172046e25
Ritesh Yadav <ry673223@gmail.com>
2025-12-18 15:07:07 +0530

    Add files via upload

---
+++ Basic Data Types in C.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    scanf("%d %ld %c %f %lf", &a, &b, &c, &d, &e);
+    printf("%d\n%ld\n%c\n%.3f\n%.9lf",a,b,c,d,e);
+    return 0;
+}
