0145537eb0cbe8823c040084104d59c86c0161cc
Ritesh Yadav <ry673223@gmail.com>
2025-12-18 15:14:03 +0530

    Add files via upload
    You are given two integers h and m, representing time in 24-hour format:
    
    h → Hour (0 to 23)
    m → Minutes (0 to 59)
    Your task is to convert this time into 12-hour format and print the result in the form:
    
    {hh}:{mm} AM/PM - where hh and mm are converted hours and minutes double digit formats
    Conversion Rules
    
    00:00 to 11:59 → AM
    12:00 to 23:59 → PM
    In 12-hour format:
    
    00 hours → 12 AM
    12 hours → 12 PM
    13–23 hours → 1-11 PM
    Minutes must always be printed as given (including leading zeros when necessary).
    
    Input Format
    
    Two space-separated integers: {h} {m}
    
    where
    
    h is the hour
    m is the minutes
    Constraints
    
    0 ≤ h ≤ 23
    0 ≤ m ≤ 59
    Output Format
    
    Print the converted time in 12-hour format as:
    
    {hh}:{mm} AM/PM , where hh and mm are converted hour and minute double digit formats
    Sample Input 0
    
    00 10
    Sample Output 0
    
    12:10 AM
    Sample Input 1
    
    15 45
    Sample Output 1
    
    03:45 PM
    Sample Input 2
    
    12 30
    Sample Output 2
    
    12:30 PM
    Sample Input 3
    
    9 45
    Sample Output 3
    
    09:45 AM

---
+++ Convert 24-Hour Time to 12-Hour Format.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int h, m;
+    scanf("%d %d", &h, &m);
+
+    int convertedHour;
+    char period[3];
+
+    if (h == 0) {
+        convertedHour = 12;
+        sprintf(period, "AM");
+    } 
+    else if (h == 12) {
+        convertedHour = 12;
+        sprintf(period, "PM");
+    }
+    else if (h > 12) {
+        convertedHour = h - 12;
+        sprintf(period, "PM");
+    }
+    else {
+        convertedHour = h;
+        sprintf(period, "AM");
+    }
+
+    printf("%d:%02d %s", convertedHour, m, period);
+    return 0;
+}
