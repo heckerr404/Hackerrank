b87a1797c899519fa66fe86bd1c0b048c2a9bfe2
Ritesh Yadav <ry673223@gmail.com>
2026-01-04 19:30:21 +0530

    Fix formatting of printf statement in loop
    Write a program that prints all the numbers between 1 and 100 (inclusive) that are divisible by both 3 and 5.
    
    Input Format
    
    N/A
    
    Constraints
    
    N/A
    
    Output Format
    
    Print all the numbers between 1 and 100 (inclusive) that are divisible by both 3 and 5, each number in a new line.

--- Loops_Divisible by 3 and 5.c
+++ Loops_Divisible by 3 and 5.c
@@ -6,7 +6,7 @@ int main() {
         int i;
          for(int i=1;i<=100;++i){
                 if(i%3==0 && i%5==0){
-                        printf("%d\n",i);
+                        printf("%d\n",i); 
                 }
         }
     return 0;
