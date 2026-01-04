a6b51947b47e2089582560d3c3360de3fc04a0e1
heckerr404 <ry668616@gmail.com>
2026-01-04 19:28:23 +0530

    new

---
+++ Loops_Divisible by 3 and 5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+        int i;
+         for(int i=1;i<=100;++i){
+                if(i%3==0 && i%5==0){
+                        printf("%d\n",i);
+                }
+        }
+    return 0;
+}
