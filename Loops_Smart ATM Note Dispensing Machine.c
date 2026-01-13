fdb9879cc2134f29c1e798c9eab59c2aa2b583ec
Ritesh Yadav <ry673223@gmail.com>
2026-01-13 20:07:46 +0530

    Add files via upload

--- Loops_Smart ATM Note Dispensing Machine.c
+++ Loops_Smart ATM Note Dispensing Machine.c
@@ -5,7 +5,7 @@
 
 int main() {
 
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    /* Enter your code here. Read input from STDIN. Print output to STDOUTS*/ 
       int A;
     scanf("%d",&A);
 
