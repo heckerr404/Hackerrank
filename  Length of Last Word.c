3906df622c00c6e970583e313c5bbd9cb5d5bba5
heckerr404 <ry673223@gmail.com>
2026-04-22 00:03:14 +0530

    commit

---
+++  Length of Last Word.c
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n=s.length();
+        int i=n-1;
+        int c=0;
+        while(i>=0 && s[i]==' '){
+            i--;
+        }
+        while(i>=0 && s[i]!=' '){
+            c++;
+            i--;
+        }
+        return c;
+          
+    }
+};
